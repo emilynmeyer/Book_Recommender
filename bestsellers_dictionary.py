933af3e89f200d4af3ade98ae5748e593269c1bc
Emily <meye2193@umn.edu>
2023-11-12 17:12:52 +0100

    Update bestsellers_dictionary.py
    Added in Google Books API description
    Run time is longer than I would like as each best seller book is called individually by ISBN in google books to retrieve data (~230 books).

--- bestsellers_dictionary.py
+++ bestsellers_dictionary.py
@@ -1,32 +1,41 @@
-'''This file reads in json data from the NYT Bestsellers API
-    it then stores information from that data in multiple dictionaries.
-    These dictionaries are Title, Author, and Description.'''
+'''This file reads in json data from the NYT Bestsellers API and Google Books API
+    it then stores information from that data in a nested dictionary.
+    The keys for this dictionary are ISBN, each ISBN has a dictionary with Title, Author, and Description.'''
 
 import requests
 import json
 
-'''with open('NYT bestsellers - full overview - current.json') as f:
-    data = json.load(f)'''
-
 NYT_API = requests.get('https://api.nytimes.com/svc/books/v3/lists/full-overview/current.json?api-key=AzHG5csxiABJYEYKFOd2hjgWF3jRNscJ')
-nyt_data = NYT_API.text
-data = json.loads(nyt_data)
+nyt = NYT_API.text
+nyt_data = json.loads(nyt)
 
-
-
-results = data['results']
+results = nyt_data['results']
 lists = results['lists']
 
-bestseller_list = [] #list of the lists of best sellers
-titles = [] #all titles in the bestsellers lists
-IBAN = [] #need unique key to look up books again
-title_IBAN = {} #look up for book titles?
-description_dict = {} #quick look up of descriptions for each book
+#bestseller_list = [] #There are multiple bestsellers lists on the NYT. This is a list of the lists of bestsellers.
+ISBN = [] #unique key to look up books
+book_list = {} #using ISBN as the key. Provides a dictionary with title, author, description
 
 for i in range(len(lists)):
-    bestseller_list.append(lists[i].get('books')) #get only the books from the bestseller list
-    for j in range(len(bestseller_list[i])):
-        title = bestseller_list[i][j].get('title') #within the book list, get the title
-        description = bestseller_list[i][j].get('description').lower() #within the book list, get the description
-        titles.append(title) #list of titles
-        description_dict[title] = description #dictionary with title as key and description as value
+    bestseller_list = lists[i].get('books') #get only the books from the bestseller list
+    for j in range(len(bestseller_list)):
+        isbn = bestseller_list[j].get('primary_isbn13')
+        if len(isbn) == 13:
+            ISBN.append(isbn)
+
+            GOOGLE_API = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
+            google = GOOGLE_API.text
+            google_data = json.loads(google)
+            if 'items' in google_data.keys():
+                google_volumeInfo = google_data['items'][0].get('volumeInfo')
+                if 'description' in google_volumeInfo.keys():
+                    google_description = google_volumeInfo['description']
+
+            title = bestseller_list[j].get('title') #within the book list, get the title
+            author = bestseller_list[j].get('author')
+            nyt_description = bestseller_list[j].get('description').lower() #within the book list, get the description
+            
+            book_list[isbn] = {}
+            book_list[isbn]['description'] = nyt_description + google_description #dictionary with title as key and description as value
+            book_list[isbn]['title'] = title
+            book_list[isbn]['author'] = author
